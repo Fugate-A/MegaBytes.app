830ebce6a6d8ca837f2295c653d1e42ab588bbc9
nathanM24 <90732881+nathanM24@users.noreply.github.com>
2023-10-18 14:00:15 -0400

    Login+Register API
    Creating Login and register api

--- server.js
+++ server.js
@@ -2,10 +2,18 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
+
+const MongoClient = require('mongodb').MongoClient;
+const url =
+	'mongodb+srv://TheArchivist:R3c1p3Guard1an5K@cluster0.7i3llee.mongodb.net/?retryWrites=true&w=majority';
+const client = new MongoClient(url);
+client.connect();
+
 const port = 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
+
 app.use((req, res, next) =>{
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader(
@@ -19,5 +27,66 @@ app.use((req, res, next) =>{
     next();
 });
 
-console.log("Listening on port "+port);
+app.post('/api/register', async (req, res, next) => {
+    // incoming: userId, fname, lname, username, password, email
+    // outgoing: error
+    const { userId, fname, lname, username, password, email } = req.body;
+    const newUser = { UserId: userId, FirstName: fname, LastName: lname, Username: username, Password: password, Email: email };
+    var error = '';
+    try {
+        const db = client.db('MegaBitesLibrary');
+        const result = db.collection('User').insertOne(newUser);
+    }
+    catch (e) {
+        error = e.toString();
+    }
+    var ret = { error: error };
+    res.status(200).json(ret);
+});
+
+app.post('/api/login', async (req, res, next) => {
+    // incoming: login, password
+    // outgoing: id, firstName, lastName, email, error
+    var error = '';
+    const { login, password } = req.body;
+    const db = client.db('MegaBitesLibrary');
+    const results = await
+        db.collection('User').find({ Login: login, Password: password }).toArray();
+    var id = -1;
+    var fn = '';
+    var ln = '';
+    var em = '';
+    if (results.length > 0) {
+        id = results[0].UserId;
+        fn = results[0].FirstName;
+        ln = results[0].LastName;
+        en = results[0].Email;
+    }
+    var ret = { id: id, firstName: fn, lastName: ln, email: em, error: '' };
+    res.status(200).json(ret);
+});
+
+app.post('/api/search', async (req, res, next) => {
+    // incoming: userId, search
+    // outgoing: results[], error
+    var error = '';
+    const { userId, search } = req.body;
+    var _search = search.trim();
+    const db = client.db('COP4331Cards');
+    const results = await
+        db.collection('Cards').find({
+            "Card": {
+                $regex: _search + '.*',
+                $options: 'r'
+            }
+        }).toArray();
+    var _ret = [];
+    for (var i = 0; i < results.length; i++) {
+        _ret.push(results[i].Card);
+    }
+    var ret = { results: _ret, error: error };
+    res.status(200).json(ret);
+});
+
+console.log("Listening on port "+ port);
 app.listen(port); // start Node + Express server on port 5000
