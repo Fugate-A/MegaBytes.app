d4583dc026c7fc66468c73e8509df8d6ddc157bb
nathanM24 <na289132@ucf.edu>
2023-10-26 01:33:47 -0400

    router config changes

--- web_frontend/src/components/Path.js
+++ web_frontend/src/components/Path.js
@@ -2,6 +2,7 @@ const app_name = 'https://www.megabytes.app/'
 exports.buildPath =
 	function buildPath(route) {
 		if (process.env.NODE_ENV === 'production') {
+			console.log(app_name + route);
 			return app_name + route;
 		}
 		else {
