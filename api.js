42fd9dbe32798f5498435261e3a5f1c3aaa843ba
Andrew Fugate <andrewfugate1121@yahoo.com>
2023-11-09 20:25:34 -0500

    Working Email API
    Sending emails under verification@megabytes.app alias works, need to merge this to main. Need to figure out next step in securing passwords, ie how to reset a password with a sent code. This is more logic based than it is based on sending an email

--- api.js
+++ api.js
@@ -1,171 +1,178 @@
-require('express');
-require('mongodb');
+const nodemailer = require('nodemailer');
+require('dotenv').config();
+
+let transporter;
 
 exports.setApp = function (app, client) {
-	app.post('/api/register', async (req, res, next) => {
-		// incoming:  username, password, email
-		// outgoing: error
-		const { username, password, email } = req.body;
-		const newUser = { UserID: Date.now(), Username: username, Password: password, Email: email };
-		var error = '';
-		try {
-			const db = client.db('MegaBitesLibrary');
-			db.collection('User').insertOne(newUser);
-		}
-		catch (e) {
-			error = e.toString();
-		}
-		var ret = { error: error };
-		res.status(200).json(ret);
-	});
+  app.post('/api/register', async (req, res, next) => {
+    // incoming: username, password, email
+    // outgoing: error
+    const { username, password, email } = req.body;
+    const newUser = { UserID: Date.now(), Username: username, Password: password, Email: email };
+    let error = '';
+    try {
+      const db = client.db('MegaBitesLibrary');
+      db.collection('User').insertOne(newUser);
+    } catch (e) {
+      error = e.toString();
+    }
+    const ret = { error: error };
+    res.status(200).json(ret);
+  });
 
-	app.post('/api/login', async (req, res, next) => {
-		// incoming: login, password
-		// outgoing: id, error
-		let error = '';
-		const { username, password } = req.body;
-		const isEmail = username.includes("@");
-		try {
-			const db = client.db('MegaBitesLibrary');
-			const results = await (isEmail
-				? db.collection('User').find({ Email: username, Password: password }).toArray()
-				: db.collection('User').find({ Username: username, Password: password }).toArray());
-			
-			var id = -1;
-			if (results.length > 0) {
-				id = results[0]._id;
-			}
-		}
-		catch (e) {
-			error = e.message()
-		}
-		let ret = { id: id, error: error };
-		res.status(200).json(ret);
-	});
+  app.post('/api/login', async (req, res, next) => {
+    // incoming: login, password
+    // outgoing: id, error
+    let error = '';
+    const { username, password } = req.body;
+    const isEmail = username.includes("@");
+    try {
+      const db = client.db('MegaBitesLibrary');
+      const results = await (isEmail
+        ? db.collection('User').find({ Email: username, Password: password }).toArray()
+        : db.collection('User').find({ Username: username, Password: password }).toArray());
+      
+      let id = -1;
+      if (results.length > 0) {
+        id = results[0]._id;
+      }
+    } catch (e) {
+      error = e.message();
+    }
+    const ret = { id: id, error: error };
+    res.status(200).json(ret);
+  });
 
-	app.post('/api/addRecipe', async (req, res, next) => {
-		// incoming: userId, recipeName, recipeContents, tagList, likeList
-		// outgoing: error
+  app.post('/api/addRecipe', async (req, res, next) => {
+    // incoming: userId, recipeName, recipeContents, tagList, likeList
+    // outgoing: error
 
-		var error = '';
-		const { userId, recipeName, recipeContents,
-			tagList, likeList } = req.body;
-		const newRecipe = {
-			UserId: userId, RecipeName: recipeName,
-			recipeContents: recipeContents,
-			TagList: tagList, LikeList: likeList
-		};
+    let error = '';
+    const { userId, recipeName, recipeContents, tagList, likeList } = req.body;
+    const newRecipe = {
+      UserId: userId, RecipeName: recipeName, recipeContents: recipeContents,
+      TagList: tagList, LikeList: likeList
+    };
 
-		try {
-			const db = client.db('MegaBitesLibrary');
-			db.collection('Recipes').insertOne(newRecipe);
-		} catch (e) {
-			error = e.toString();
-		}
+    try {
+      const db = client.db('MegaBitesLibrary');
+      db.collection('Recipes').insertOne(newRecipe);
+    } catch (e) {
+      error = e.toString();
+    }
 
-		var ret = { error: error };
-		res.status(200).json(ret);
-	});
+    const ret = { error: error };
+    res.status(200).json(ret);
+  });
 
-	app.post('/api/deleteRecipe', async (req, res, next) => {
-		// incoming: recipeId
-		// outgoing: error
+  app.post('/api/deleteRecipe', async (req, res, next) => {
+    // incoming: recipeId
+    // outgoing: error
 
-		var error = '';
-		const { recipeId } = req.body;
-		const filter = { RecipeId: recipeId };
+    let error = '';
+    const { recipeId } = req.body;
+    const filter = { RecipeId: recipeId };
+    const db = client.db('MegaBitesLibrary');
+    db.collection('Recipes').deleteOne(filter, (err, result) => {
+      if (err) {
+        console.error('Error deleting document:', err);
+      } else {
+        console.log('Deleted document successfully');
+      }
+    });
 
-		const db = client.db('MegaBitesLibrary');
-		db.collection('Recipes').deleteOne(filter, (err, result) => {
-			if (err) {
-				console.error('Error deleting document:', err);
-			} else {
-				console.log('Deleted document successfully');
-			}
-		});
+    const ret = { error: error };
+    res.status(200).json(ret);
+  });
 
-		var ret = { error: error };
-		res.status(200).json(ret);
-	});
+  app.post('/api/getRecipes', async (req, res, next) => {
+    // incoming: search
+    // outgoing: results[], error
 
-	app.post('/api/getRecipes', async (req, res, next) => {
-		// incoming: search
-		// outgoing: results[], error
+    let error = '';
+    const { search } = req.body;
+    const _search = search.trim();
+    const db = client.db('MegaBitesLibrary');
+    const results = await db.collection('Recipes').find({
+      "RecipeName": {
+        $regex: _search + '.*',
+        $options: 'i'
+      }
+    }).toArray();
 
-		var error = '';
-		const { search } = req.body;
-		var _search = search.trim();
-		const db = client.db('MegaBitesLibrary');
-		const results = await
-			db.collection('Recipes').find({
-				"RecipeName": {
-					$regex: _search + '.*',
-					$options: 'i'
-				}
-			}).toArray();
+    const ret = { results: results, error: error };
+    res.status(200).json(ret);
+  });
 
-		var ret = { results: results, error: error };
-		res.status(200).json(ret);
-	});
+  app.post('/api/addComment', async (req, res, next) => {
+    // incoming: recipeId, userId, commentId, commentText
+    // outgoing: error
 
-	app.post('/api/addComment', async (req, res, next) => {
-		// incoming: recipeId, userId, commentId, commentText
-		// outgoing: error
+    let error = '';
+    const { recipeId, userId, commentId, commentText } = req.body;
+    const newComment = {
+      RecipeId: recipeId, UserId: userId, CommentId: commentId, CommentText: commentText
+    };
+    try {
+      const db = client.db('MegaBitesLibrary');
+      db.collection('Comments').insertOne(newComment);
+    } catch (e) {
+      error = e.toString();
+    }
 
-		var error = '';
-		const { recipeId, userId, commentId, commentText } = req.body;
-		const newComment = {
-			RecipeId: recipeId, UserId: userId, CommentId: commentId, CommentText: commentText
-		};
-		try {
-			const db = client.db('MegaBitesLibrary');
-			db.collection('Comments').insertOne(newComment);
-		} catch (e) {
-			error = e.toString();
-		}
+    const ret = { error: error };
+    res.status(200).json(ret);
+  });
 
-		var ret = { error: error };
-		res.status(200).json(ret);
-	});
+  app.post('/api/deleteComment', async (req, res, next) => {
+    // incoming: recipeId
+    // outgoing: error
 
-	app.post('/api/deleteComment', async (req, res, next) => {
-		// incoming: recipeId
-		// outgoing: error
+    let error = '';
+    const { commentId } = req.body;
+    const filter = { CommentId: commentId };
+    const db = client.db('MegaBitesLibrary');
+    db.collection('Comments').deleteOne(filter, (err, result) => {
+      if (err) {
+        console.error('Error deleting document:', err);
+      } else {
+        console.log('Deleted document successfully');
+      }
+    });
 
-		var error = '';
-		const { commentId } = req.body;
-		const filter = { CommentId: commentId };
-		const db = client.db('MegaBitesLibrary');
-		db.collection('Comments').deleteOne(filter, (err, result) => {
-			if (err) {
-				console.error('Error deleting document:', err);
-			} else {
-				console.log('Deleted document successfully');
-			}
-		});
+    const ret = { error: error };
+    res.status(200).json(ret);
+  });
 
-		var ret = { error: error };
-		res.status(200).json(ret);
-	});
+  transporter = nodemailer.createTransport({
+    host: 'smtp.forwardemail.net',
+    port: 587,
+    secure: false,
+    auth: {
+      user: process.env.VerificationEmail,
+      pass: process.env.VerificationEmailPassword
+    },
+    from: process.env.VerificationEmail
+  });
 
-	/*
-	app.post('/api/getComments', async (req, res, next) => {
-		// incoming: search
-		// outgoing: results[], error
+  app.post('/api/sendEmail', (req, res) => {
+    const { to, subject, text } = req.body;
 
-		var error = '';
-		const { search } = req.body;
-		var _search = search.trim();
-		const db = client.db('MegaBitesLibrary');
-		const results = await
-			db.collection('Comments').find({
-				"RecipeName": {
-					$regex: _search + '.*',
-					$options: 'i'
-				}
-			}).toArray();
+    const mailOptions = {
+      from: process.env.VerificationEmail,
+      to,
+      subject,
+      text
+    };
 
-		var ret = { results: results, error: error };
-		res.status(200).json(ret);
-	});*/
-}
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.error('Error sending email:', error);
+        res.status(500).send('Error sending email');
+      } else {
+        console.log('Email sent: ' + info.response);
+        res.status(200).send('Email sent successfully');
+      }
+    });
+  });
+};
