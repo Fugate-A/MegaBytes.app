e921b1b2ff2e8d154f5c7e19f57da41af66d5368
NickAwrist <nicholas.aristizabal@gmail.com>
2023-11-18 00:59:14 +0000

    added await to addComment

--- api.js
+++ api.js
@@ -319,7 +319,7 @@ exports.setApp = function (app, client) {
 		try {
 			const db = client.db('MegaBitesLibrary');
 
-			const insertResult = db.collection('Comments').insertOne(newComment);
+			const insertResult = await db.collection('Comments').insertOne(newComment);
 
 			const commentId = insertResult.insertedId;
 
@@ -331,7 +331,7 @@ exports.setApp = function (app, client) {
 
 			console.log(updateResult);
 
-			res.status(200).json({ error: null });
+			res.status(200).json({ commentId: commentId, error: null });
 		} catch (error) {
 			console.error(error);
 			res.status(500).json({ error: 'Internal Server Error' });
