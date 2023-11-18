24b78acdc96a4b2e5f2f7bcc89c1a5775313c6f7
nathanM24 <90732881+nathanM24@users.noreply.github.com>
2023-11-17 19:30:03 -0500

    transition updates

--- web_frontend/src/components/Index.js
+++ web_frontend/src/components/Index.js
@@ -5,7 +5,7 @@ function IndexInfo() {
 		<div className='bg-page-background indexdiv flex items-center justify-center h-screen bg-orange-300'>
 			<div className="flex-1 flex-col justify-center px-6 py-12 lg:px-8">
 				<div className="sm:mx-auto sm:w-max">
-					<a href='http://localhost:3000/'>
+					<a href='https://www.megabytes.app/'>
 						<h2 className="mt-10 text-center text-8xl font-bold leading-9 tracking-wide text-gray-50 content-center 
 									   transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-125 duration-300">
 							READY TO COOK!
