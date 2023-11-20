3acbecb025979b045636154e21b7984b386a49ed
Andrew Fugate <andrewfugate1121@yahoo.com>
2023-11-20 14:43:26 -0500

    email works entirly, just left with visual issues

--- web_frontend/src/components/Register.js
+++ web_frontend/src/components/Register.js
@@ -4,48 +4,56 @@ function Register() {
   const [message, setMessage] = useState('');
 
   const sendVerificationEmail = async (username, password, email) => {
-	const obj = { username, password, email };
-	const js = JSON.stringify(obj);
-  
-	try {
-	  const response = await fetch(bp.buildPath('api/verifyEmail'), {
-		method: 'POST',
-		body: js,
-		headers: {
-		  'Content-Type': 'application/json'
-		}
-	  });
-  
-	  const res = JSON.parse(await response.text());
-  
-	  if (res.error.length > 0) {
-		setMessage(res.error);
-	  } else {
-		// Assuming that 'res.token' contains the verification token
-		const verificationLink = `${window.location.origin}/verify/${res.token}`;
-		setMessage('Verification email sent successfully.');
-		console.log('Verification Link:', verificationLink);
-		// You can display the verification link or send it via email
-	  }
-	} catch (e) {
-	  console.error('Error sending verification email:', e);
-	}
-  };
-  
+    const obj = { username, password, email };
+    const js = JSON.stringify(obj);
 
-  const registerUser = async (event) => {
+    try {
+      const response = await fetch('http://localhost:5000/api/verifyEmail', { // Ensure this URL matches your server URL
+        method: 'POST',
+        body: js,
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+
+      const res = await response.json();
+
+      if (res.error && res.error.length > 0) {
+        setMessage(res.error);
+      } else {
+        setMessage('Verification email sent successfully.');
+      }
+    } catch (e) {
+      console.error('Error sending verification email:', e);
+      setMessage('Error sending verification email. Please try again.');
+    }
+  };
+
+  const registerUser = (event) => {
     event.preventDefault();
     const regUsername = event.target.username.value;
     const regPassword = event.target.password.value;
-    const regCPassword = event.target.cpassword.value;
+    const regEmail = event.target.email.value;
 
-    sendVerificationEmail(regUsername, regPassword, regCPassword);
+    sendVerificationEmail(regUsername, regPassword, regEmail);
   };
 
   return (
     <div id="registerDiv">
       <form onSubmit={registerUser}>
-        {/* Your registration form JSX code here */}
+        <div>
+          <label>Username:</label>
+          <input type="text" name="username" required />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input type="password" name="password" required />
+        </div>
+        <div>
+          <label>Email:</label>
+          <input type="email" name="email" required />
+        </div>
+        <button type="submit">Register</button>
       </form>
       <span id="regResult">{message}</span>
     </div>
