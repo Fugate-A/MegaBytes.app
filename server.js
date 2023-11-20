0aada64567f23c2b9f1248868f8afc7466c3a2df
NickAwrist <nicholas.aristizabal@gmail.com>
2023-11-20 21:51:51 +0000

    Added AI endpoint

--- server.js
+++ server.js
@@ -21,9 +21,11 @@ client.connect();
 
 // Import your API configuration
 var api = require('./api.js'); // Adjust the path as needed
+let ai = require('./ai_request.js');
 
 // Set up the API routes
 api.setApp(app, client); // Adjust the client as needed
+ai.setApp(app, client);
 
 app.use((req, res, next) => {
 	res.setHeader('Access-Control-Allow-Origin', '*');
@@ -42,16 +44,16 @@ app.use((req, res, next) => {
 app.use(express.static(path.join(__dirname, 'web_frontend', 'build')));
 
 // Create an HTTPS server with SSL configuration
-const httpsOptions = {
-	key: fs.readFileSync('/etc/ssl/private/generated-private-key.key'),
-	cert: fs.readFileSync('/etc/ssl/certs/2541c4c881b019c0.crt'),
-	ca: [
-		fs.readFileSync('/etc/ssl/certs/2541c4c881b019c0.crt'),
-		fs.readFileSync('/etc/ssl/certs/gd_bundle-g2-g1.crt'),
-	],
-};
+// const httpsOptions = {
+// 	key: fs.readFileSync('/etc/ssl/private/generated-private-key.key'),
+// 	cert: fs.readFileSync('/etc/ssl/certs/2541c4c881b019c0.crt'),
+// 	ca: [
+// 		fs.readFileSync('/etc/ssl/certs/2541c4c881b019c0.crt'),
+// 		fs.readFileSync('/etc/ssl/certs/gd_bundle-g2-g1.crt'),
+// 	],
+// };
 
-const httpsServer = https.createServer(httpsOptions, app);
+//const httpsServer = https.createServer(httpsOptions, app);
 
 // Serve the React application for both root URL and "/megabytes.app"
 app.get('*', (req, res) => {
@@ -59,9 +61,9 @@ app.get('*', (req, res) => {
 });
 
 // Start the HTTPS server on port 443
-httpsServer.listen(httpsPort, () => {
-	console.log(`HTTPS Server is running on port ${httpsPort}`);
-});
+// httpsServer.listen(httpsPort, () => {
+// 	console.log(`HTTPS Server is running on port ${httpsPort}`);
+// });
 
 // Start the HTTP server on port 5000
 app.listen(httpPort, () => {
