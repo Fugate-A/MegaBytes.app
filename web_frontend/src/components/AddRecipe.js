ec758720badf24295d6a030a2a569281dd19448c
Briman225 <briman225@gmail.com>
2023-11-28 17:35:46 -0500

    Removed junk comments

--- web_frontend/src/components/AddRecipe.js
+++ web_frontend/src/components/AddRecipe.js
@@ -198,62 +198,6 @@ function AddRecipe() {
 				</div>}
 			</div>
 		</div >
-
-
-
-
-
-
-
-		/*{<div className="container mx-auto p-4">
-			<button onClick={handleAddRecipe} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-				Submit
-			</button>
-
-			<div className="mt-4 p-4 bg-white shadow-md rounded-md">
-				<input
-					type="text"
-					placeholder="Title"
-					value={title}
-					onChange={(e) => setTitle(e.target.value)}
-					className="w-full h-12 p-2 mb-4 border-b-2 border-black"
-				/>
-
-				<textarea
-					placeholder="Ingredients and Directions"
-					value={content}
-					onChange={(e) => setContent(e.target.value)}
-					className="w-full h-32 p-2 mb-4 border-2 border-gray-400 rounded"
-					rows="4"
-				/>
-
-				<button
-					onClick={openTagSelectionModal}
-					className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded"
-				>
-					Add Tags
-				</button>
-
-				{showTagSelectionModal && (
-					<TagSelectionModal
-						visible={true}
-						onUpdateRecipeTags={handleUpdateRecipeTags}
-						onClose={() => setShowTagSelectionModal(false)}
-						currentTags={recipeTags}
-					/>
-				)}
-
-				<div className="flex items-center mt-4">
-					<span className="mr-2 font-bold">Visibility:</span>
-					<button onClick={toggleVisibility} className="flex items-center">
-						<div className={`w-4 h-4 rounded-full border border-black mr-2 ${visibility ? 'bg-green-500' : 'bg-white'}`} />
-						<span>{visibility ? 'Public' : 'Private'}</span>
-					</button>
-				</div>
-			</div>
-
-			<ErrorMessageModal visible={showErrorModal} message={errorMessage} onClose={closeErrorModal} />
-		</div> }*/
 	);
 };
 export default AddRecipe;
