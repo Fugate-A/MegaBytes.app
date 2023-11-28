5da2f5957b12e61113f016179d2d6aa70e3ad4d3
Briman225 <briman225@gmail.com>
2023-11-27 19:49:42 -0500

    Updated AddRecipe

--- web_frontend/src/App.js
+++ web_frontend/src/App.js
@@ -14,24 +14,24 @@ import VerifyEmail from './components/VerifyEmail'; // Import the VerifyEmail co
 import AddRecipePage from './components/AddRecipe';
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/rec" element={<RecipePage />} />
-        <Route path="/reg" element={<RegisterPage />} />
-        <Route path="/i" element={<IndexPage />} />
-        <Route path="/cre" element={<CreatePage />} />
-        <Route path="/com" element={<CommunityPage />} />
-        <Route path="/verify" element={<VerifyEmail />} />
-        <Route path="/forgotPassword" element={<ForgotPasswordPage />} />
-        <Route path="/resetPassword" element={<ResetPasswordPage />} />
-        <Route path="/dis/:recipeInfo" element={<DisplayRecipe />} />
-        {/* Add a route for the VerifyEmail component with a query parameter */}
-        <Route path="/addCustomRecipe" element={<AddRecipePage />} />
-      </Routes>
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<LoginPage />} />
+				<Route path="/rec" element={<RecipePage />} />
+				<Route path="/reg" element={<RegisterPage />} />
+				<Route path="/i" element={<IndexPage />} />
+				<Route path="/cre" element={<CreatePage />} />
+				<Route path="/com" element={<CommunityPage />} />
+				<Route path="/verify" element={<VerifyEmail />} />
+				<Route path="/forgotPassword" element={<ForgotPasswordPage />} />
+				<Route path="/resetPassword" element={<ResetPasswordPage />} />
+				<Route path="/dis/:recipeInfo" element={<DisplayRecipe />} />
+				{/* Add a route for the VerifyEmail component with a query parameter */}
+				<Route path="/addCustomRecipe" element={<AddRecipePage />} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
--- web_frontend/src/components/AddRecipe.js
+++ web_frontend/src/components/AddRecipe.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import ErrorMessageModal from '../components/ErrorMessageModal';
 import TagSelectionModal from '../components/TagSelectionModal';
+import NavBar from '../components/Navbar';
 function AddRecipe() {
 	const [title, setTitle] = useState('');
 	const [content, setContent] = useState('');
@@ -77,7 +78,70 @@ function AddRecipe() {
 	}
 
 	return (
-		<div className="container mx-auto p-4">
+		<div id="AddCustomDiv" className='h-screen bg-orange-300'>
+			<NavBar />
+			<h1 className="text-6xl font-bold leading-9 tracking-tight text-black p-5 bg-orange-300">
+				Create a Custom Recipe
+			</h1>
+			<div className="mt-2 mb-3 flex justify-center">
+				{<div className="container mx-auto p-4">
+					<div className="mt-4 p-4 bg-white shadow-md rounded-md">
+						<input
+							type="text"
+							placeholder="Title"
+							value={title}
+							onChange={(e) => setTitle(e.target.value)}
+							className="w-full h-12 p-2 mb-4 border-b-2 border-black"
+						/>
+
+						<textarea
+							placeholder="Ingredients and Directions"
+							value={content}
+							onChange={(e) => setContent(e.target.value)}
+							className="w-full h-32 p-2 mb-4 border-2 border-gray-400 rounded"
+							rows="4"
+						/>
+
+						<button
+							onClick={openTagSelectionModal}
+							className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded"
+						>
+							Add Tags
+						</button>
+
+						{showTagSelectionModal && (
+							<TagSelectionModal
+								visible={true}
+								onUpdateRecipeTags={handleUpdateRecipeTags}
+								onClose={() => setShowTagSelectionModal(false)}
+								currentTags={recipeTags}
+							/>
+						)}
+
+						<div className="flex items-center mt-4">
+							<span className="mr-2 font-bold">Visibility:</span>
+							<button onClick={toggleVisibility} className="flex items-center">
+								<div className={`w-4 h-4 rounded-full border border-black mr-2 ${visibility ? 'bg-green-500' : 'bg-white'}`} />
+								<span>{visibility ? 'Public' : 'Private'}</span>
+							</button>
+						</div>
+						<button onClick={handleAddRecipe} className="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded">
+							Submit
+						</button>
+					</div>
+
+					<ErrorMessageModal visible={showErrorModal} message={errorMessage} onClose={closeErrorModal} />
+				</div>}
+			</div>
+		</div >
+
+
+
+
+
+
+
+		/*{<div className="container mx-auto p-4">
 			<button onClick={handleAddRecipe} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
 				Submit
 			</button>
@@ -125,7 +189,7 @@ function AddRecipe() {
 			</div>
 
 			<ErrorMessageModal visible={showErrorModal} message={errorMessage} onClose={closeErrorModal} />
-		</div>
+		</div> }*/
 	);
 };
 export default AddRecipe;
