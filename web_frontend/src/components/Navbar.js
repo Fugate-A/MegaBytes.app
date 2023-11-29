8bac37219067413e74eafcd2619bb1e3b5581114
Andrew Fugate <andrewfugate1121@yahoo.com>
2023-11-29 17:19:39 -0500

    updated profile

--- web_frontend/src/components/Navbar.js
+++ web_frontend/src/components/Navbar.js
@@ -93,19 +93,7 @@ export default function NavBar() {
 									>
 										<Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
 											{/* Dropdown Menu Items */}
-											<Menu.Item>
-												{({ active }) => (
-													<a
-														href="/profile"
-														className={classNames(
-															active ? 'bg-gray-100' : '',
-															'block px-4 py-2 text-sm text-gray-700'
-														)}
-													>
-														My Profile
-													</a>
-												)}
-											</Menu.Item>
+											
 											<Menu.Item>
 												{({ active }) => (
 													<a
