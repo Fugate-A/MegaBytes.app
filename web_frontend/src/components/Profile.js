e7e8f3d3804fd0ff62c9965e6fe73c6c1fb86a7c
Andrew Fugate <andrewfugate1121@yahoo.com>
2023-11-28 19:12:40 -0500

    profile page works except missing user data

--- web_frontend/src/components/Profile.js
+++ web_frontend/src/components/Profile.js
@@ -17,17 +17,17 @@ const Profile = () => {
 
   return (
     <div id="combinedDiv" className="flex min-h-full flex-col items-center justify-start bg-beige px-4 py-8 bg-page-background pt-20 min-h-screen">
-      <h2 className="text-3xl font-bold text-black">Megabytes</h2>
+      <h2 className="text-3xl font-bold text-black">User Information:</h2>
       <div className="mt-6 w-full max-w-xs rounded-lg bg-orange-100 border-4 border-black p-4">
         <div className="mb-4">
-          <h3 className="text-lg font-bold text-gray-900">Username</h3>
+          <h3 className="text-lg font-bold text-gray-900">Username:</h3>
           <div className="mt-2 rounded-md bg-white p-2 text-lg text-gray-900">
             {user.username}
           </div>
         </div>
 
         <div className="mb-6">
-          <h3 className="text-lg font-bold text-gray-900">Email address</h3>
+          <h3 className="text-lg font-bold text-gray-900">Email:</h3>
           <div className="mt-2 rounded-md bg-white p-2 text-lg text-gray-900">
             {user.email}
           </div>
@@ -35,7 +35,8 @@ const Profile = () => {
 
         <button
           onClick={doLogout}
-          className="w-full rounded-md bg-yellow-600 px-4 py-2 text-lg font-semibold text-white hover:bg-yellow-700"
+          style={{ backgroundColor: '#E79B11' }}
+          className="w-full rounded-md px-4 py-2 text-lg font-semibold text-white hover:bg-yellow-700"
         >
           Logout
         </button>
