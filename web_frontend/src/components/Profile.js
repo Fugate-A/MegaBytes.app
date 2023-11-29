06b2dfb497de9ae41ce4de96e77a0c35d300cb42
Andrew Fugate <andrewfugate1121@yahoo.com>
2023-11-29 16:25:29 -0500

    profile border

--- web_frontend/src/components/Profile.js
+++ web_frontend/src/components/Profile.js
@@ -18,7 +18,7 @@ const Profile = () => {
   return (
     <div id="combinedDiv" className="flex min-h-full flex-col items-center justify-start bg-beige px-4 py-8 bg-page-background pt-20 min-h-screen">
       <h2 className="text-3xl font-bold text-black">User Information:</h2>
-      <div className="mt-6 w-full max-w-xs rounded-lg bg-orange-100 border-4 border-black p-4">
+      <div className="mt-6 w-full max-w-xs rounded-lg bg-orange-100 border-2 border-black p-4">
         <div className="mb-4">
           <h3 className="text-lg font-bold text-gray-900">Username:</h3>
           <div className="mt-2 rounded-md bg-white p-2 text-lg text-gray-900">
