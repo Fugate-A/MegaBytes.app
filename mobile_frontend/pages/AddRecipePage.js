065def3b9418f5d184a9079660a8c96f481a683f
Briman225 <briman225@gmail.com>
2023-11-28 21:49:43 -0500

    Tags

--- mobile_frontend/pages/AddRecipePage.js
+++ mobile_frontend/pages/AddRecipePage.js
@@ -9,344 +9,342 @@ import TagSelectionModal from '../components/TagSelectionModal';
 import AIRequestModal from '../components/AIRequestModal';
 
 
-function AddRecipePage(){
-    const navigation = useNavigation();
+function AddRecipePage() {
+	const navigation = useNavigation();
 
-    const [title, setTitle] = useState('');
-    const [content, setContent] = useState('');
-    const [recipeTags, setRecipeTags] = useState([]);
-    const [visibility, setVisibility] = useState(false);
-    const [AIgenerated, setAIgenerated] = useState(false);
+	const [title, setTitle] = useState('');
+	const [content, setContent] = useState('');
+	const [recipeTags, setRecipeTags] = useState([]);
+	const [visibility, setVisibility] = useState(false);
+	const [AIgenerated, setAIgenerated] = useState(false);
 
-    const [showErrorModal, setShowErrorModal] = useState(false);
-    const [errorMessage, setErrorMessage] = useState('');
-    const [showTagSelectionModal, setShowTagSelectionModal] = useState(false);
-    const [showAIModal, setShowAIModal] = useState(false);
-    const [isScrolling, setIsScrolling] = useState(false);
-    const [isTextInputFocused, setIsTextInputFocused] = useState(false);
+	const [showErrorModal, setShowErrorModal] = useState(false);
+	const [errorMessage, setErrorMessage] = useState('');
+	const [showTagSelectionModal, setShowTagSelectionModal] = useState(false);
+	const [showAIModal, setShowAIModal] = useState(false);
+	const [isScrolling, setIsScrolling] = useState(false);
+	const [isTextInputFocused, setIsTextInputFocused] = useState(false);
 
 
-    const [userID, setUserID] = useState(null);
-    useEffect( () => {
-        const fetchUserID = async () => {
-            try {
-                const storedUserID = await AsyncStorage.getItem('userID');
-                setUserID(storedUserID);
-            } catch (error) {
-                console.error('Error retrieving userID from cache', error);
-            }
-        };
+	const [userID, setUserID] = useState(null);
+	useEffect(() => {
+		const fetchUserID = async () => {
+			try {
+				const storedUserID = await AsyncStorage.getItem('userID');
+				setUserID(storedUserID);
+			} catch (error) {
+				console.error('Error retrieving userID from cache', error);
+			}
+		};
 
-        fetchUserID();
-    }, []);
-    
-    const handleAddRecipe = async () => {
-        
-        try {
-            const response = await fetch('http://164.90.130.112:5000/api/addRecipe', {
+		fetchUserID();
+	}, []);
+
+	const handleAddRecipe = async () => {
+
+		try {
+			const response = await fetch('http://164.90.130.112:5000/api/addRecipe', {
 				method: 'POST',
 				headers: {
 					'Content-Type': 'application/json',
 				},
 
 				body: JSON.stringify({
-                    userId: userID,
-                    recipeName: title,
-                    recipeContents: content,
-                    tagList: recipeTags || [],
-                    likeList: [],
-                    isPublic: visibility,
-                    ai_generated: AIgenerated,
+					userId: userID,
+					recipeName: title,
+					recipeContents: content,
+					tagList: recipeTags || [],
+					likeList: [],
+					isPublic: visibility,
+					ai_generated: AIgenerated,
 				}),
-		});
+			});
 
-            console.log('Adding Recipe');
-            const data = await response.json();
+			console.log('Adding Recipe');
+			const data = await response.json();
 
-            if(response.ok){
-                console.log('Success');
+			if (response.ok) {
+				console.log('Success');
 
-                navigation.navigate('Home');
-            }else{
-                console.error('Error adding Recipe');
+				navigation.navigate('Home');
+			} else {
+				console.error('Error adding Recipe');
 
-                setErrorMessage('Error adding Reicpe');
+				setErrorMessage('Error adding Reicpe');
 				setShowErrorModal(true);
-            }
-        } catch(error) {
-            console.error("ERROR CONNECTING TO DATABASE\n", error);
-        }
-        
+			}
+		} catch (error) {
+			console.error("ERROR CONNECTING TO DATABASE\n", error);
+		}
+	};
 
-    };
+	const handleUpdateRecipeTags = (updatedTags) => {
+		setRecipeTags(updatedTags);
+	}
 
-    const handleUpdateRecipeTags = (updatedTages) => {
-        setRecipeTags(updatedTages);
-    }
-
-    const handleAIInput = (updatedTitle, updatedContent) => {
-        setTitle(updatedTitle);
-        setContent(updatedContent);
-        setAIgenerated(true);
-    }
+	const handleAIInput = (updatedTitle, updatedContent) => {
+		setTitle(updatedTitle);
+		setContent(updatedContent);
+		setAIgenerated(true);
+	}
 
 	const closeErrorModal = () => {
 		setShowErrorModal(false);
 	};
 
-    const closeAIModal = () => {
-        setShowAIModal(false);
-    }
+	const closeAIModal = () => {
+		setShowAIModal(false);
+	}
 
-    const openAIModal = () => {
-        setShowAIModal(true);
-    }
+	const openAIModal = () => {
+		setShowAIModal(true);
+	}
 
-    const openTagSelectionModal = () => {
-        setShowTagSelectionModal(true);
-    }
+	const openTagSelectionModal = () => {
+		setShowTagSelectionModal(true);
+	}
 
-    const toggleVisibility = () => {
-        setVisibility(!visibility);
-    }
+	const toggleVisibility = () => {
+		setVisibility(!visibility);
+	}
 
-    const dismissKeyboard = () => {
-        Keyboard.dismiss();
-    };
+	const dismissKeyboard = () => {
+		Keyboard.dismiss();
+	};
 
-    const handleScrollBegin = () => {
-        setIsScrolling(true);
-    };
-    
-    const handleScrollEnd = () => {
-        setIsScrolling(false);
-    };
-    
-    
+	const handleScrollBegin = () => {
+		setIsScrolling(true);
+	};
 
-    return (
-        <ScrollView 
-            style={styles.container}
-        >
-            <TouchableWithoutFeedback onPress={dismissKeyboard}>
-                
-                <View>
-                    <View style={styles.topButtons}>
-                        <TouchableOpacity onPress={openAIModal} style={styles.aigenButton}>
-                            <Text style={styles.aigenButtonText}>Generate with AI 🤖</Text>
-                        </TouchableOpacity>
+	const handleScrollEnd = () => {
+		setIsScrolling(false);
+	};
 
-                        {showAIModal && (
-                            <AIRequestModal
-                                visible={showAIModal} 
-                                onClose={closeAIModal}
-                                handleAIInput={handleAIInput}
-                            />
-                        )}
-                        
-                        <TouchableOpacity 
-                            onPress={handleAddRecipe}
-                            style={styles.submittButton}
-                        >
-                            <Text style={styles.submittButtonText}>Submit</Text>
-                        </TouchableOpacity>
-                    </View>
 
-                    <View style={styles.inputContainer}>
-                        <TextInput
-                            placeholder='Title'
-                            value = {title}
-                            onChangeText={(text) => setTitle(text)}
-                            style={styles.titleInput}
-                            editable={!isScrolling}
-                        />
 
-                        <View 
-                            style={styles.contentInputContainer}
-                        >
-                            <TextInput
-                                placeholder='Ingredients and Directions'
-                                value = {content}
-                                onChangeText={(text) => setContent(text)}
-                                style={[styles.contentInput, isTextInputFocused ? { height: 'auto' } : null]}
-                                multiline
-                                editable={!isScrolling}
-                                scrollEnabled={!isScrolling}
-                            />
-                        </View>
+	return (
+		<ScrollView
+			style={styles.container}
+		>
+			<TouchableWithoutFeedback onPress={dismissKeyboard}>
 
-                        <TouchableOpacity onPress={openTagSelectionModal} style={styles.addTagsButton}>
-                            <MaterialCommunityIcons name='tag-plus-outline' size={30} color='black' />
-                            <Text style={styles.addTagsText}>Add Tags</Text>
-                        </TouchableOpacity>
-                        
-                        {showTagSelectionModal && (
-                            <TagSelectionModal
-                                visible={true}
-                                onUpdateRecipeTags={handleUpdateRecipeTags}
-                                onClose={() => setShowTagSelectionModal(false)}
-                                currentTags={recipeTags}
-                            />
-                        )}
+				<View>
+					<View style={styles.topButtons}>
+						<TouchableOpacity onPress={openAIModal} style={styles.aigenButton}>
+							<Text style={styles.aigenButtonText}>Generate with AI 🤖</Text>
+						</TouchableOpacity>
 
-                        <View style={styles.visibilityContainer}>
-                            <Text style={styles.visibilityLabel}>Privacy:</Text>
+						{showAIModal && (
+							<AIRequestModal
+								visible={showAIModal}
+								onClose={closeAIModal}
+								handleAIInput={handleAIInput}
+							/>
+						)}
 
-                            <View style={styles.visibilityTextContainer}>
-                                <TouchableOpacity onPress={toggleVisibility} style={styles.visibilityButton}>
-                                    <View style={[styles.radioCircle, { backgroundColor: visibility ? 'green' : 'white' }]} />
-                                </TouchableOpacity>
-                                <Text style={styles.visibilityLabel}>{visibility ? 'Public' : 'Private'}</Text>
-                            </View>
+						<TouchableOpacity
+							onPress={handleAddRecipe}
+							style={styles.submittButton}
+						>
+							<Text style={styles.submittButtonText}>Submit</Text>
+						</TouchableOpacity>
+					</View>
 
-                            <View style={styles.visibilityIconContainer}>
-                                {visibility ? (
-                                    <MaterialCommunityIcons name='lock-open-outline' size={35} color='black' />
-                                ) : (
-                                    <MaterialCommunityIcons name='lock-outline' size={35} color='black' />
-                                )}
-                            </View>
+					<View style={styles.inputContainer}>
+						<TextInput
+							placeholder='Title'
+							value={title}
+							onChangeText={(text) => setTitle(text)}
+							style={styles.titleInput}
+							editable={!isScrolling}
+						/>
 
-                        </View>
+						<View
+							style={styles.contentInputContainer}
+						>
+							<TextInput
+								placeholder='Ingredients and Directions'
+								value={content}
+								onChangeText={(text) => setContent(text)}
+								style={[styles.contentInput, isTextInputFocused ? { height: 'auto' } : null]}
+								multiline
+								editable={!isScrolling}
+								scrollEnabled={!isScrolling}
+							/>
+						</View>
 
-                    </View>
+						<TouchableOpacity onPress={openTagSelectionModal} style={styles.addTagsButton}>
+							<MaterialCommunityIcons name='tag-plus-outline' size={30} color='black' />
+							<Text style={styles.addTagsText}>Add Tags</Text>
+						</TouchableOpacity>
 
-                    <ErrorMessageModal
-                        visible={showErrorModal}
-                        message={errorMessage}
-                        onClose={closeErrorModal}
-                    />
-                </View>
-                
-            </TouchableWithoutFeedback>
-        </ScrollView>
-    );
+						{showTagSelectionModal && (
+							<TagSelectionModal
+								visible={true}
+								onUpdateRecipeTags={handleUpdateRecipeTags}
+								onClose={() => setShowTagSelectionModal(false)}
+								currentTags={recipeTags}
+							/>
+						)}
+
+						<View style={styles.visibilityContainer}>
+							<Text style={styles.visibilityLabel}>Privacy:</Text>
+
+							<View style={styles.visibilityTextContainer}>
+								<TouchableOpacity onPress={toggleVisibility} style={styles.visibilityButton}>
+									<View style={[styles.radioCircle, { backgroundColor: visibility ? 'green' : 'white' }]} />
+								</TouchableOpacity>
+								<Text style={styles.visibilityLabel}>{visibility ? 'Public' : 'Private'}</Text>
+							</View>
+
+							<View style={styles.visibilityIconContainer}>
+								{visibility ? (
+									<MaterialCommunityIcons name='lock-open-outline' size={35} color='black' />
+								) : (
+									<MaterialCommunityIcons name='lock-outline' size={35} color='black' />
+								)}
+							</View>
+
+						</View>
+
+					</View>
+
+					<ErrorMessageModal
+						visible={showErrorModal}
+						message={errorMessage}
+						onClose={closeErrorModal}
+					/>
+				</View>
+
+			</TouchableWithoutFeedback>
+		</ScrollView>
+	);
 
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignContent: 'center',
-        backgroundColor: '#FFF0DC',
-        padding: 10,
-        paddingTop: 40,
-    },
-    topButtons: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        padding: 5,
-    },
-    aigenButtonText: {
-        fontSize: 14,
-        fontFamily: 'Tilt-Neon',
-    },
-    submittButtonText: {
-        fontSize: 14,
-        fontFamily: 'Tilt-Neon',
-    },
-    aigenButton: {
-        marginRight: 15,
-        borderWidth: 1,
-        borderRadius: 15,
-        padding: 10,
-        backgroundColor: '#51E4E5',
-    },
-    submittButton: {
-        marginRight: 15,
-        borderWidth: 1,
-        borderRadius: 15,
-        padding: 10,
-        backgroundColor: '#51E564',
-    },
-    inputContainer: {
-        marginTop: -15,
-        padding: 10,
-    },
-    titleInput: {
-        backgroundColor: '#FFE6C5',
-        borderRadius: 15,
-        width: '100%',
-        height: 80,
-        marginVertical: 10,
-        padding: 8,
-        fontSize: 30,
-        fontFamily: 'Tilt-Neon',
+	container: {
+		flex: 1,
+		alignContent: 'center',
+		backgroundColor: '#FFF0DC',
+		padding: 10,
+		paddingTop: 40,
+	},
+	topButtons: {
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		padding: 5,
+	},
+	aigenButtonText: {
+		fontSize: 14,
+		fontFamily: 'Tilt-Neon',
+	},
+	submittButtonText: {
+		fontSize: 14,
+		fontFamily: 'Tilt-Neon',
+	},
+	aigenButton: {
+		marginRight: 15,
+		borderWidth: 1,
+		borderRadius: 15,
+		padding: 10,
+		backgroundColor: '#51E4E5',
+	},
+	submittButton: {
+		marginRight: 15,
+		borderWidth: 1,
+		borderRadius: 15,
+		padding: 10,
+		backgroundColor: '#51E564',
+	},
+	inputContainer: {
+		marginTop: -15,
+		padding: 10,
+	},
+	titleInput: {
+		backgroundColor: '#FFE6C5',
+		borderRadius: 15,
+		width: '100%',
+		height: 80,
+		marginVertical: 10,
+		padding: 8,
+		fontSize: 30,
+		fontFamily: 'Tilt-Neon',
 		fontWeight: 'bold',
-        borderBottomColor: 'black',
-        borderBottomWidth: 2,
-    },
-    contentInputContainer: {
-        backgroundColor: '#FFE6C5',
-        height: '65%',
-        borderRadius: 15,
-        borderLeftColor: 'grey',
-        borderRightColor: 'grey',
-        borderLeftWidth: 1,
-        borderRightWidth: 1,
-    },
-    contentInput: {
-        width: '100%',
-        marginVertical: 10,
-        padding: 8,
-        fontFamily: 'Tilt-Neon',
-        padding: 10,
-    },
-    addTagsText: {
-        fontSize: 20,
-        fontFamily: 'Tilt-Neon',
-        marginLeft: 10,
-        color: 'gray',
-    },
-    addTagsButton: {
-        flexDirection: 'row',
-        backgroundColor: '#FFE6C5',
-        borderWidth: 0.5,
-        borderRadius: 15,
-        padding: 10,
-        marginTop: 10,
-        width: '60%',
-    },
-    visibilityContainer: {
-        backgroundColor: '#FFE6C5',
-        flexDirection: 'row',
-        alignItems: 'center',
-        marginTop: 10,
+		borderBottomColor: 'black',
+		borderBottomWidth: 2,
+	},
+	contentInputContainer: {
+		backgroundColor: '#FFE6C5',
+		height: '65%',
+		borderRadius: 15,
+		borderLeftColor: 'grey',
+		borderRightColor: 'grey',
+		borderLeftWidth: 1,
+		borderRightWidth: 1,
+	},
+	contentInput: {
+		width: '100%',
+		marginVertical: 10,
+		padding: 8,
+		fontFamily: 'Tilt-Neon',
+		padding: 10,
+	},
+	addTagsText: {
+		fontSize: 20,
+		fontFamily: 'Tilt-Neon',
+		marginLeft: 10,
+		color: 'gray',
+	},
+	addTagsButton: {
+		flexDirection: 'row',
+		backgroundColor: '#FFE6C5',
+		borderWidth: 0.5,
+		borderRadius: 15,
+		padding: 10,
+		marginTop: 10,
+		width: '60%',
+	},
+	visibilityContainer: {
+		backgroundColor: '#FFE6C5',
+		flexDirection: 'row',
+		alignItems: 'center',
+		marginTop: 10,
 
-        width: 250,
-        padding: 10,
-        borderRadius: 15,
-        borderWidth: 0.5,
-    },
-    visibilityTextContainer: {
-        flexDirection: 'row', 
-        alignItems: 'center', 
-        width: 150 
-    },
-    visibilityIconContainer: {
-        flexDirection: 'row', 
-        alignItems: 'center', 
-        width: 35,
-        marginLeft: -30,
-    },
-    visibilityLabel: {
-        fontSize: 16,
+		width: 250,
+		padding: 10,
+		borderRadius: 15,
+		borderWidth: 0.5,
+	},
+	visibilityTextContainer: {
+		flexDirection: 'row',
+		alignItems: 'center',
+		width: 150
+	},
+	visibilityIconContainer: {
+		flexDirection: 'row',
+		alignItems: 'center',
+		width: 35,
+		marginLeft: -30,
+	},
+	visibilityLabel: {
+		fontSize: 16,
 
-        marginRight: 10,
-        fontFamily: 'Tilt-Neon',
-    },
-    visibilityButton: {
-        padding: 10,
-        flexDirection: 'row',
-        alignItems: 'center',
-    },
-    radioCircle: {
-        width: 20,
-        height: 20,
-        borderRadius: 10,
-        borderWidth: 1,
-        marginRight: 5,
-    },
+		marginRight: 10,
+		fontFamily: 'Tilt-Neon',
+	},
+	visibilityButton: {
+		padding: 10,
+		flexDirection: 'row',
+		alignItems: 'center',
+	},
+	radioCircle: {
+		width: 20,
+		height: 20,
+		borderRadius: 10,
+		borderWidth: 1,
+		marginRight: 5,
+	},
 });
 
 export default AddRecipePage;
--- web_frontend/src/components/TagSelectionModal.js
+++ web_frontend/src/components/TagSelectionModal.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import TagComponent from './TagComponent';
 
 function TagSelectionModal({ visible, onClose, onUpdateRecipeTags, currentTags }) {
+<<<<<<< Updated upstream
   const [tags, setTags] = useState([]);
   const [recipeTags, setRecipeTags] = useState(currentTags);
 
@@ -20,6 +21,28 @@ function TagSelectionModal({ visible, onClose, onUpdateRecipeTags, currentTags }
         console.error('Error connecting to server', error);
       }
     };
+=======
+	const [tags, setTags] = useState([]);
+	const [recipeTags, setRecipeTags] = useState(currentTags || []);
+	const [loading, setLoading] = useState(true);
+	useEffect(() => {
+		const fetchTags = async () => {
+			try {
+				const response = await fetch('http://164.90.130.112:5000/api/tags');
+				const data = await response.json();
+
+				if (response.ok) {
+					setTags(data);
+				} else {
+					console.error('Error retrieving tags from server');
+				}
+			} catch (error) {
+				console.error('Error connecting to server', error);
+			} finally {
+				setLoading(false);
+			}
+		};
+>>>>>>> Stashed changes
 
     fetchTags();
   }, []);
@@ -28,6 +51,7 @@ function TagSelectionModal({ visible, onClose, onUpdateRecipeTags, currentTags }
     onUpdateRecipeTags(recipeTags);
   }, [recipeTags, onUpdateRecipeTags]);
 
+<<<<<<< Updated upstream
   const toggleTagSelection = (tagIndex) => {
     if (recipeTags.includes(tagIndex)) {
       setRecipeTags(recipeTags.filter((index) => index !== tagIndex));
@@ -53,6 +77,16 @@ function TagSelectionModal({ visible, onClose, onUpdateRecipeTags, currentTags }
 	  </button>
     );
   };
+=======
+	const toggleTagSelection = (tagIndex) => {
+		if (recipeTags && recipeTags.includes(tagIndex)) {
+			setRecipeTags(recipeTags.filter(index => index !== tagIndex));
+			console.log(recipeTags);
+		} else {
+			setRecipeTags([...recipeTags, tagIndex]);
+			console.log(recipeTags);
+		}
+>>>>>>> Stashed changes
 
   return (
     <div className={`fixed inset-0 flex items-center justify-center ${visible ? 'visible' : 'invisible'}`}>
@@ -74,4 +108,43 @@ function TagSelectionModal({ visible, onClose, onUpdateRecipeTags, currentTags }
   );
 }
 
+<<<<<<< Updated upstream
 export default TagSelectionModal;
+=======
+	if (loading) {
+		return null;
+	}
+
+	return (
+		<div className={`fixed inset-0 flex items-center justify-center ${visible ? 'opacity-100' : 'opacity-0 pointer-events-none'}`}>
+			<div className="bg-white p-12 rounded-lg w-128 max-h-80 overflow-y-auto overflow-x-hidden shadow-lg">
+				<div className="flex justify-between items-center mb-4 border-b-2 border-black pb-2">
+					<h2 className="text-xl font-semibold text-gray-800">Tags</h2>
+					<button className="p-2 cursor-pointer" onClick={onClose}>
+						<span className="text-gray-600">X</span>
+					</button>
+				</div>
+				<div className="grid grid-cols-3 gap-4 mt-4">
+					{tags.map((item, index) => (
+						<button
+							key={index}
+							className={`mx-2 mb-2 overflow-hidden ${recipeTags.includes(index) ? 'bg-gray-300' : ''}`}
+							onClick={() => toggleTagSelection(index)}
+						>
+							<TagComponent name={item.name} emoji={item.emoji} color={item.color} isSelected={recipeTags.includes(index)} />
+						</button>
+					))}
+				</div>
+				<div className="mt-4">{JSON.stringify(recipeTags)}</div>
+			</div>
+		</div>
+	);
+
+
+
+
+
+}
+
+export default TagSelectionModal;
+>>>>>>> Stashed changes
