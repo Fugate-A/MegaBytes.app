8b1710c23bb28bfa1bcb465658751d2ffffe5b44
nickaristizabal <nicholas.aristizabal@gmail.com>
2023-11-29 02:25:58 -0500

    Tag Selection modal styling and fix

--- web_frontend/src/components/TagComponent.js
+++ web_frontend/src/components/TagComponent.js
@@ -3,8 +3,8 @@ import React from "react";
 function TagComponent({ name, emoji, color, isSelected }) {
 	return (
 		<div
-			className={`mt-10 w-140 rounded-full px-10 py-5 mr-10 border-0.5 ${isSelected ? 'opacity-50 bg-gray-500' : ''}`}
-			style={{ backgroundColor: color }}
+			className={`mt-10 w-140 rounded-full px-10 py-5 mr-10 border-0.5 ${isSelected ? 'opacity-30 bg-gray-500' : ''}`}
+			style={{ backgroundColor: color, borderWidth: 2, borderColor: '#000' }}
 		>
 			<span className="text-black text-14 font-Tilt-Neon">{emoji} {name}</span>
 		</div>
