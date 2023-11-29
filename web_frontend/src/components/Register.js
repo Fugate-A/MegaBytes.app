980639e08a51b7d5b342835502cb653fa5e3f69a
Andrew Fugate <andrewfugate1121@yahoo.com>
2023-11-28 22:49:13 -0500

    updating reg text

--- web_frontend/src/components/Register.js
+++ web_frontend/src/components/Register.js
@@ -3,12 +3,13 @@ import { useNavigate } from 'react-router-dom';
 
 function Register() {
   const [message, setMessage] = useState('');
+  const [isPasswordValid, setIsPasswordValid] = useState(false);
   const navigate = useNavigate();
 
   const sendVerificationEmail = async (username, password, email) => {
     const obj = { username, password, email };
     const js = JSON.stringify(obj);
-  
+
     try {
       const response = await fetch('https://megabytes.app/api/verifyEmail', {
         method: 'POST',
@@ -17,7 +18,7 @@ function Register() {
           'Content-Type': 'application/json'
         }
       });
-  
+
       if (response.ok && response.headers.get("content-type")?.includes("application/json")) {
         var res = await response.json();
         if (res.error) {
@@ -36,7 +37,14 @@ function Register() {
       return;
     }
   };
-  
+
+  const validatePassword = (password) => {
+    const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/;
+    const isValid = passwordRegex.test(password);
+    setIsPasswordValid(isValid);
+    return isValid;
+};
+
 
   const registerUser = (event) => {
     event.preventDefault();
@@ -44,78 +52,81 @@ function Register() {
     const regPassword = event.target.password.value;
     const regEmail = event.target.email.value;
 
+    if (!validatePassword(regPassword)) {
+      setMessage('Please review the password requirments and try again.');
+      return;
+  }
+  
+
     sendVerificationEmail(regUsername, regPassword, regEmail);
   };
 
   return (
-    <div id="registerDiv">
-      <form onSubmit={registerUser}>
-        <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-          <div className="sm:mx-auto sm:w-full sm:max-w-md">
-            <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-neutral-950">
-              Register
-            </h2>
-          </div>
-
-          <div className="mt-4 p-3 sm:mx-auto sm:w-full sm:max-w-md bg-orange-100 border-4 border-neutral-950 rounded-lg">
-            <div className="space-y-6">
-              <div>
-                <label htmlFor="username" className="block text-sm font-medium leading-6 text-gray-900">
-                  Username
-                </label>
-                <div className="mt-2">
-                  <input
-                    type="text"
-                    id="username"
-                    name="username"
-                    required
-                    placeholder="Username"
-                    className="appearance-none block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm placeholder:text-gray-400 focus:ring-2 focus:ring-indigo-500 sm:text-sm"
-                  />
-                </div>
-              </div>
-              <div>
-                <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
-                  Password
-                </label>
-                <div className="mt-2">
-                  <input
-                    type="password"
-                    id="password"
-                    name="password"
-                    required
-                    placeholder="Password"
-                    className="appearance-none block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm placeholder:text-gray-400 focus:ring-2 focus:ring-indigo-500 sm:text-sm"
-                  />
-                </div>
-              </div>
-              <div>
-                <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
-                  Email
-                </label>
-                <div className="mt-2">
-                  <input
-                    type="email"
-                    id="email"
-                    name="email"
-                    required
-                    placeholder="Email"
-                    className="appearance-none block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm placeholder:text-gray-400 focus:ring-2 focus:ring-indigo-500 sm:text-sm"
-                  />
-                </div>
-              </div>
-              <div>
-                <button
-                  type="submit"
-                  className="flex w-full justify-center rounded-md bg-orange-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:ring-indigo active:bg-indigo-700"
-                >
-                  Register
-                </button>
-              </div>
-            </div>
-          </div>
+    <div id="registerDiv" className="flex justify-center items-center h-screen">
+      <form onSubmit={registerUser} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        <div className="mb-4">
+          <label htmlFor="username" className="block text-gray-700 text-sm font-bold mb-2">
+            Username
+          </label>
+          <input
+            type="text"
+            id="username"
+            name="username"
+            required
+            placeholder="Username"
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          />
         </div>
-        <span id="registerResult">{message}</span>
+        <div className="mb-4">
+          <label htmlFor="password" className="block text-gray-700 text-sm font-bold mb-2">
+            Password
+          </label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            required
+            placeholder="Password"
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+            onChange={(e) => validatePassword(e.target.value)}
+          />
+          <p className={`text-xs italic ${isPasswordValid ? 'text-green-500' : 'text-red-500'}`}>
+    {isPasswordValid ? 'Password is valid' : (
+        <>
+            Password must: <br />
+            - Be 8 characters long <br />
+            - Include at least 1 uppercase letter <br />
+            - Include at least 1 lowercase letter <br />
+            - Include at least 1 number
+        </>
+    )}
+</p>
+
+        </div>
+        <div className="mb-6">
+          <label htmlFor="email" className="block text-gray-700 text-sm font-bold mb-2">
+            Email
+          </label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            required
+            placeholder="Email Address"
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+          />
+        </div>
+        <div className="flex items-center justify-between">
+          <button
+            type="submit"
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          >
+            Register
+          </button>
+          <span id="registerResult" className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800">
+            {message}
+          </span>
+        </div>
       </form>
     </div>
   );
